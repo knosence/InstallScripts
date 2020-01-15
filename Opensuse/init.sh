de104f42a06d655fb35e98cac5d614a2aad1fdb2
knosence <nadario.seays@gmail.com>
2020-01-15 02:26:17 -0500

    updates file names, refactored code to reflectt script names.

--- Opensuse/init.sh
+++
@@ -1,4 +0,0 @@
-echo '****************************starting install Script****************************'
-source $HOME/Scripts/ChromeInstall.sh
-source $HOME/Scripts/CSCodeInstall.sh
-source $HOME/Scripts/NeededInstall.sh
