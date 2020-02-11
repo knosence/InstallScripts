4db88dcb0777d9873c8a1b03a95d1b440269967b
Kno Sence <kSep@localhost.localdomain>
2020-02-10 20:27:02 -0500

    added a symlink to install to set it automatically durring install.

--- installer_scripts/full_install.sh
+++ installer_scripts/full_install.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+echo -e 'Creating a Symlink for forge to usr/local/bin'
+
+ln -s ~/GitRepos/scripts/forge /usr/local/bin/forge
+
 echo -e 'Starting Full Install'
 
 source $HOME/GitRepo/scripts/installer_scripts/base_package_install.sh
