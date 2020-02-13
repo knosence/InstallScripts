a57477936203c3afe26657413c5cbecdd0a805c6
Kno Sence <kSep@localhost.localdomain>
2020-02-13 06:24:59 -0500

    adds more packages to install for nvidia

--- installer_scripts/base_package_install.sh
+++ installer_scripts/base_package_install.sh
@@ -7,14 +7,16 @@ echo -e 'Starting With Installing Repos'
 echo -e 'Installing RPMFusion'
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+echo -e 'Installing Nivida Drivers'
+sudo dnf install akmod-nvidia
 
 echo -e 'Installing Fedora Workstation Repositores'
 sudo dnf install fedora-workstation-repositories-31-1.fc31.noarch -y
 
 echo -e 'Installing Multimedia'
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-
-echo -e 'Installing Multimedia'
 sudo dnf groupupdate sound-and-video
 
 
