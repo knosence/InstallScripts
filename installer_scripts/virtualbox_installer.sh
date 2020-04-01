e7248bb164feb3d2c63ae19a335ae8aaa89ba684
Kno Sence <kSep@localhost.localdomain>
2020-03-31 21:20:58 -0400

    adds virtualbox installer

--- installer_scripts/virtualbox_installer.sh
+++ installer_scripts/virtualbox_installer.sh
@@ -14,17 +14,20 @@ baseurl=http://download.virtualbox.org/virtualbox/rpm/fedora/29/\$basearch
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
-gpgkey=http://www.virtualbox.org/downlaod/oracle_vbox.asc
+gpgkey=https://www.virtualbox.org/downlaod/oracle_vbox.asc
 EOF
 
 echo -e 'Importing Virtualbox GPG keys'
 sudo dnf search virtualbox -y
 
 echo -e 'Installing Virtualbox 6'
-sudo dnf install virtualBox-6.1
+sudo dnf install VirtualBox
 
 echo -e 'Adding your user to the Vboxusers Group'
-sudo usermod -a -G vboxusers &USER
+sudo usermod -a -G vboxusers kSep
 
 newgrp vboxusers
 id $USER
+
+sudo dnf install akmod-VirtualBox kernel-devel-$(uname -r)
+sudo kmods; systemctl restart systemd-modules-load.service
